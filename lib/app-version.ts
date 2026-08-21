28f56776d30a497c1c21b40914c3fbb284f80dc7
Hidehisa Alexander Fujioka <alex@hot-jpn.com>
2026-08-21 12:47:33 +0900

    Pin Node 24 for SHINROMii

--- lib/app-version.ts
+++ lib/app-version.ts
@@ -1,4 +1,4 @@
 /** Single source of truth for the version shown in the UI. */
-export const APP_VERSION = "0.99.4";
+export const APP_VERSION = "0.99.5";
 
 export const APP_VERSION_LABEL = `Ver.${APP_VERSION}`;
